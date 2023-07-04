c6c68d69bfc6fdbaac2fd5917066b634cae4b558
ganeshsakle <akhilesh.patel@gustazos.com>
2023-07-04 12:11:55 +0530

    Next permutation LC31 solution

--- next_permutation.cpp
+++ next_permutation.cpp
@@ -1,25 +1,34 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-void nextPermutation(int arr[], int n){
-    int flag = 0;
-    for(int i=0; i<n; i++){
-        for(int j=n-2; j>=0; j--){
-        if(arr[i] > arr[j]){
-            swap(arr[i], arr[j]);
-            flag = 1;
+void nextPermutation(vector<int>& nums) {
+    int flag = -1;
+    int n = nums.size();
+    for(int i=n-2; i>=0; i--){
+        if(nums[i] < nums[i+1]){
+            flag = i;
             break;
         }
-        }
-        if(flag == 1) break;
     }
+    if(flag == -1){
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+    for(int i=n-1; i>flag; i--){
+        if(nums[i] > nums[flag]){
+            swap(nums[i], nums[flag]);
+            break;
+        }
+    }
+    reverse(nums.begin()+flag+1, nums.end());
 }
 
 int main(){
-    int arr[] = {1,2};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    nextPermutation(arr,n);
-    for(int i=0; i<n; i++){
+    vector<int> arr = {2, 1, 5, 4, 3, 0, 0};
+    nextPermutation(arr);
+    for(int i=0; i<arr.size(); i++){
         cout<<arr[i]<<" ";
     }
     return 0;
